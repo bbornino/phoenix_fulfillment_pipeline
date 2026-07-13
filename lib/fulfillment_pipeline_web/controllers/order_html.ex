94dff1a3c0321364b305a5cc90282eb45e3a9b51
Brian Bornino <brianbornino@gmail.com>
2026-07-13 11:19:29 -0700

    Add status colors to orders index and show

--- lib/fulfillment_pipeline_web/controllers/order_html.ex
+++ lib/fulfillment_pipeline_web/controllers/order_html.ex
@@ -2,4 +2,12 @@ defmodule FulfillmentPipelineWeb.OrderHTML do
   use FulfillmentPipelineWeb, :html
 
   embed_templates "order_html/*"
+
+  def status_color("received"), do: "color: #6b7280"
+  def status_color("picking"), do: "color: #d97706"
+  def status_color("packing"), do: "color: #d97706"
+  def status_color("shipping"), do: "color: #2563eb"
+  def status_color("delivered"), do: "color: #16a34a"
+  def status_color("exception"), do: "color: #dc2626"
+  def status_color(_), do: "color: #6b7280"
 end
